8ba29c414fe6d2dbfb3c1941a352ac2b85afc4c7
xPiLoYx <rexs.llemel333@gmail.com>
2024-12-08 20:07:04 +0800

    add watchnowbutton

--- src/components/WatchNowButton.tsx
+++ src/components/WatchNowButton.tsx
@@ -12,7 +12,10 @@ export function WatchNowButton() {
       href="/home"
       onClick={(e) => {
         e.preventDefault(); // Prevent the default link behavior
-        window.open('https://anotherlink', '_blank'); // Open the second link in a new tab
+        window.open(
+          'https://www.profitablecpmrate.com/esxakj3b?key=4fa27388d230df37216df8c1510e10ba',
+          '_blank',
+        ); // Open the second link in a new tab
         window.location.href = '/home'; // Navigate to the original link
       }}>
       Watch Now <ArrowRight className="ml-1 inline-block" />

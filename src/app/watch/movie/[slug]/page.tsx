c82a8f3c34bb94731abf196a586446b2cdf5f816
Rexsovel <rexs.llemel333@gmail.com>
2025-06-08 01:20:32 +0800

    changed source

--- src/app/watch/movie/[slug]/page.tsx
+++ src/app/watch/movie/[slug]/page.tsx
@@ -5,6 +5,6 @@ export const revalidate = 3600;
 
 export default function Page({ params }: { params: { slug: string } }) {
   const id = params.slug.split('-').pop();
-  // return <EmbedPlayer url={`https://vidsrc.to/embed/movie/${id}`} />;
-  return <EmbedPlayer url={`https://vidsrc.cc/v3/embed/movie/${id}`} />;
+  return <EmbedPlayer url={`https://vidsrc.to/embed/movie/${id}`} />;
+  // return <EmbedPlayer url={`https://vidsrc.cc/v3/embed/movie/${id}`} />;
 }

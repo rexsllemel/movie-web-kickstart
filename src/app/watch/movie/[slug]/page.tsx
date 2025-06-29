fbf6254bb7b9962ae71f031734ba0e15a43ce1eb
Rexsovel <rexs.llemel333@gmail.com>
2025-06-29 18:28:24 +0800

    new srcs

--- src/app/watch/movie/[slug]/page.tsx
+++ src/app/watch/movie/[slug]/page.tsx
@@ -5,6 +5,8 @@ export const revalidate = 3600;
 
 export default function Page({ params }: { params: { slug: string } }) {
   const id = params.slug.split('-').pop();
-  return <EmbedPlayer url={`https://vidsrc.to/embed/movie/${id}`} />;
+  return (
+    <EmbedPlayer url={`https://player.videasy.net/movie/${id}?color=006c48`} />
+  );
   // return <EmbedPlayer url={`https://vidsrc.cc/v3/embed/movie/${id}`} />;
 }

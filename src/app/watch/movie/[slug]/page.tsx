31adc058255b3c2452831ac7097d8da79bcaa4e7
rexsllemel <rexs.llemel333@gmail.com>
2025-07-03 23:36:02 +0800

    changed source

--- src/app/watch/movie/[slug]/page.tsx
+++ src/app/watch/movie/[slug]/page.tsx
@@ -6,7 +6,8 @@ export const revalidate = 3600;
 export default function Page({ params }: { params: { slug: string } }) {
   const id = params.slug.split('-').pop();
   return (
-    <EmbedPlayer url={`https://player.videasy.net/movie/${id}?color=006c48`} />
+    <EmbedPlayer url={`https://vidsrc.cc/v3/embed/movie/${id}`} />
+    // <EmbedPlayer url={`https://player.videasy.net/movie/${id}?color=006c48`} />
   );
   // return <EmbedPlayer url={`https://vidsrc.cc/v3/embed/movie/${id}`} />;
 }

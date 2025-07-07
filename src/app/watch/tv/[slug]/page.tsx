b0280005b1d940fc5a71b8c3ddca7a117bb8fd9d
Rexsovel <rexs.llemel333@gmail.com>
2025-07-08 03:07:43 +0800

    changed source

--- src/app/watch/tv/[slug]/page.tsx
+++ src/app/watch/tv/[slug]/page.tsx
@@ -5,6 +5,10 @@ export const revalidate = 3600;
 
 export default function Page({ params }: { params: { slug: string } }) {
   const id = params.slug.split('-').pop();
-  // return <EmbedPlayer url={`https://vidsrc.to/embed/tv/${id}`} />;
-  return <EmbedPlayer url={`https://vidsrc.cc/v3/embed/tv/${id}`} />;
+  return (
+    <EmbedPlayer
+      url={`https://player.videasy.net/tv/${id}?nextEpisode=true&autoplayNextEpisode=true&episodeSelector=true&color=006c48`}
+    />
+  );
+  // return <EmbedPlayer url={`https://vidsrc.cc/v3/embed/tv/${id}`} />;
 }
